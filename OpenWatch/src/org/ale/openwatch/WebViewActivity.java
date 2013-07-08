0ddb07ef0a56d611cb656092b25f13d592b5a5a9
David Brodsky <davidpbrodsky@gmail.com>
2013-07-08 14:17:22 -0700

    handle redirects in webview

--- OpenWatch/src/org/ale/openwatch/WebViewActivity.java
+++ OpenWatch/src/org/ale/openwatch/WebViewActivity.java
@@ -1,13 +1,11 @@
 package org.ale.openwatch;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.actionbarsherlock.app.SherlockActivity;
-import org.ale.openwatch.R;
-import org.ale.openwatch.constants.Constants;
 
 /**
  * Created by davidbrodsky on 7/5/13.
@@ -23,6 +21,14 @@ public class WebViewActivity extends SherlockActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
         webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url);
+                return false; // then it is not handled by default action
+            }
+        });
         setUrl(getUrlFromIntent(getIntent()));
     }
 
