12affe2e76ca2bd1032d2a7a4f3a52c0911e58d4
David Brodsky <davidpbrodsky@gmail.com>
2013-07-11 00:17:43 -0700

    visual polishing

--- OpenWatch/src/org/ale/openwatch/OWProfileActivity.java
+++ OpenWatch/src/org/ale/openwatch/OWProfileActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -57,6 +58,7 @@ public class OWProfileActivity extends FaceBookSherlockActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_profile);
         firstName = (TextView) findViewById(R.id.firstName);
         lastName = (TextView) findViewById(R.id.lastName);
@@ -69,8 +71,10 @@ public class OWProfileActivity extends FaceBookSherlockActivity {
         lastNameSet = false;
         blurbSet = false;
 
-        if(TwitterUtils.isAuthenticated(this))
+        if(TwitterUtils.isAuthenticated(this)){
             twitterButton.setText(getString(R.string.disconnect));
+            //findViewById(R.id.socialTip).setVisibility(View.INVISIBLE);
+        }
 
         twitterButton.setOnClickListener(twitterButtonClickListener);
 
@@ -259,7 +263,9 @@ public class OWProfileActivity extends FaceBookSherlockActivity {
             case R.id.menu_save:
                 this.finish();
                 break;
-
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
         }
         return true;
     }
